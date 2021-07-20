124d56f07e4a292d460e8f4640a2ab4bd4f6ee58
SherlonFra <80291777+SherlonFra@users.noreply.github.com>
2021-07-20 17:52:22 -0400

    moved

---
+++ RestaurantManager/Data/Migrations/20210709095024_ShoppingCart.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestaurantManager.Data.Migrations
+{
+    public partial class ShoppingCart : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShoppingCarts",
+                columns: table => new
+                {
+                    ShoppingCartId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AspNetUserId = table.Column<int>(type: "int", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Purchased = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShoppingCarts", x => x.ShoppingCartId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MenuitemShoppingCart",
+                columns: table => new
+                {
+                    ItemsMenuItemId = table.Column<int>(type: "int", nullable: false),
+                    ShoppingCartsShoppingCartId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MenuitemShoppingCart", x => new { x.ItemsMenuItemId, x.ShoppingCartsShoppingCartId });
+                    table.ForeignKey(
+                        name: "FK_MenuitemShoppingCart_Menuitems_ItemsMenuItemId",
+                        column: x => x.ItemsMenuItemId,
+                        principalTable: "Menuitems",
+                        principalColumn: "MenuItemId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MenuitemShoppingCart_ShoppingCarts_ShoppingCartsShoppingCartId",
+                        column: x => x.ShoppingCartsShoppingCartId,
+                        principalTable: "ShoppingCarts",
+                        principalColumn: "ShoppingCartId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuitemShoppingCart_ShoppingCartsShoppingCartId",
+                table: "MenuitemShoppingCart",
+                column: "ShoppingCartsShoppingCartId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MenuitemShoppingCart");
+
+            migrationBuilder.DropTable(
+                name: "ShoppingCarts");
+        }
+    }
+}
---
+++ RestaurantManager/Models/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantManager.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+
+        [Required]
+
+       
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Quantity { get; set; }
+
+        public decimal Total { get; set; }
+
+        public decimal TotalPrice {get; set; }
+    }
+}
